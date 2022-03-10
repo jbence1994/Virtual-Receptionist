caed0f65c2b3454533b2ad373f1e676100b646e3
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-03-10 10:48:33 +0100

    Remove clutters

--- VirtualReceptionist.Desktop/Repositories/BillingRepository.cs
+++ VirtualReceptionist.Desktop/Repositories/BillingRepository.cs
@@ -4,37 +4,15 @@ using VirtualReceptionist.Desktop.Models;
 
 namespace VirtualReceptionist.Desktop.Repositories
 {
-    /// <summary>
-    /// Számlázás adattár
-    /// </summary>
     public class BillingRepository : Repository
     {
-        #region Adattagok
+        private readonly List<BillingItem> billingItems;
 
-        /// <summary>
-        /// Számlázási tételeket tartalmazó lista
-        /// </summary>
-        private List<BillingItem> billingItems;
-
-        #endregion
-
-        #region Konstruktor
-
-        /// <summary>
-        /// Számlázás adattár konstruktora
-        /// </summary>
         public BillingRepository()
         {
             billingItems = new List<BillingItem>();
         }
 
-        #endregion
-
-        #region Adatfeltöltő metódusok
-
-        /// <summary>
-        /// Metódus, amely adatbázisból feltölti a számlázási tételek adatait tartalmazó listát
-        /// </summary>
         private void UploadBillingItemsList()
         {
             string sql =
@@ -55,14 +33,6 @@ namespace VirtualReceptionist.Desktop.Repositories
             }
         }
 
-        #endregion
-
-        #region Adatelérési metódusok
-
-        /// <summary>
-        /// Metódus, amely feltölti a számlázási tételeket tartalmazó listát adatbázisból
-        /// </summary>
-        /// <returns>Az adatokkal feltöltött listával tér vissza a metódus</returns>
         public List<BillingItem> GetBillingItems()
         {
             if (billingItems.Count == 0)
@@ -73,43 +43,21 @@ namespace VirtualReceptionist.Desktop.Repositories
             return billingItems;
         }
 
-        #endregion
-
-        #region Üzleti logika
-
-        /// <summary>
-        /// Metódus, amely tétel kedvezményt számít
-        /// </summary>
-        /// <param name="itemPrice">Tétel értéke, amelyből kedvezményt számol a függvény</param>
-        ///<param name="footPercent">Százalékláb értéke</param>
-        /// <returns>A kiszámolt kedvezmény értékével tér vissza a függvény</returns>
         public double CountDiscountPrice(double itemPrice, double footPercent)
         {
             double difference = (itemPrice * footPercent) / 100;
             return itemPrice - difference;
         }
 
-        /// <summary>
-        /// Metódus, amely kiszámolja a fizetendő végösszeget
-        /// </summary>
-        /// <param name="price">Tétel egységár</param>
-        /// <param name="quantity">Tétel mennyiség</param>
-        /// <returns>Fizetendő végösszeget adja vissza a függvény</returns>
         public double CountTotalPrice(double price, int quantity)
         {
             return price * quantity;
         }
 
-        /// <summary>
-        /// Metódus, amely számla nyomtatás esetén beállítja az adatbázisban fizetettre az adott foglalást
-        /// </summary>
-        /// <param name="booking">Számlázott foglalás</param>
         public void SetBookingAsPaid(Booking booking)
         {
             string sql = $"UPDATE booking SET booking.Paid = 1 WHERE booking.ID = \"{booking.Id}\"";
             Database.Dml(sql);
         }
-
-        #endregion
     }
 }
