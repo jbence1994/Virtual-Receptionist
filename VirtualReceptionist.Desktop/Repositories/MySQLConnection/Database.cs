f832168fdc141359e8822aa2a2f880c7f1661717
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2022-05-21 00:08:57 +0200

    Revert "Fix database connection (#3)"
    This reverts commit 9cb9fbbb8e145ad9bd7ae1f2de7be5de09d44a97.

--- VirtualReceptionist.Desktop/Repositories/MySQLConnection/Database.cs
+++ VirtualReceptionist.Desktop/Repositories/MySQLConnection/Database.cs
@@ -78,14 +78,14 @@ namespace virtual_receptionist.Repositories.MySQLConnection
             {
                 mySqlConnection = new MySqlConnection()
                 {
-                    ConnectionString = "SERVER=127.0.0.1; DATABASE=virtual_receptionist; UID=root; PASSWORD=mySQLserver!12345; PORT=3306; SslMode=None;"
+                    ConnectionString = ConfigurationManager.ConnectionStrings["local"].ConnectionString
                 };
             }
             else if (connectionType == "iskolai")
             {
                 mySqlConnection = new MySqlConnection()
                 {
-                    ConnectionString = "SERVER=10.0.128.111; DATABASE=zarodolgozat_2018szjuhben; UID=zarodolgozat; PASSWORD=zarodolgozat; PORT=3306; SslMode=None;"
+                    ConnectionString = ConfigurationManager.ConnectionStrings["remote"].ConnectionString
                 };
             }
             else
